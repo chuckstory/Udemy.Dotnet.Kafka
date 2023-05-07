513a9c1a714a472a3a30284c101917ec291586a5
chuckstory <poowadon.ingeni@gmail.com>
2023-05-07 15:32:57 +0700

    End Of Section 9

---
+++ SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Query.Domain.Entities
+{
+    [Table("Comment")]
+    public class CommentEntity
+    {
+        [Key]
+        public Guid CommentId { get; set; }
+        public string Username { get; set; }
+        public DateTime CommentDate { get; set; }
+        public string Comment { get; set; }
+        public bool Edited { get; set; }
+        public Guid PostId { get; set; }
+
+        [System.Text.Json.Serialization.JsonIgnore] // This is to prevent a circular reference when serializing to JSON
+        public virtual PostEntity Post { get; set; }
+    }
+}
